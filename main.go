727c5b7e254d491251ac586b0196c7007caa7c54
deepthakannan <deepthakannan@gmail.com>
2019-08-29 11:13:35 -0400

    a GET url with path params

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	// "net/http"
+	"net/http"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run(":3000")
+
+	router.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message);
+	})
+	router.Run(":3000")
 }
