ee3a161f7eccbdaa42d748a0fdc7b1c7fe712ee5
deepthakannan <deepthakannan@gmail.com>
2019-08-29 15:28:07 -0400

    skip binding validation

--- handlers/loginHandler.go
+++ handlers/loginHandler.go
@@ -2,20 +2,22 @@ package handlers
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 type Login struct {
-	User string `json:"username" binding:"required"`
+	User     string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
+	Zone     string `json:"Zone" binding:"-"`
 }
 
 func LoginHandler(context *gin.Context) {
 	var login Login
 	if err := context.ShouldBindJSON(&login); err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{ "error": err.Error()})
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	
-	context.JSON(http.StatusOK, gin.H{"status": login.User + ", you are logged in"})
+
+	context.JSON(http.StatusOK, gin.H{"status": login.User + ", you are logged in " + login.Zone})
 }
