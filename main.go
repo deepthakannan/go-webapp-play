f2bed7fa7d8373ccd72466b26eb474d5b3e81e20
deepthakannan <deepthakannan@gmail.com>
2019-08-29 11:50:45 -0400

    parse query string in urls

--- main.go
+++ main.go
@@ -7,17 +7,31 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+	router.GET("/ping", func(context *gin.Context) {
+		context.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-	router.GET("/user/:name/*action", func(c *gin.Context) {
-		name := c.Param("name")
-		action := c.Param("action")
+	router.GET("/user/:name/*action", func(context *gin.Context) {
+		name := context.Param("name")
+		action := context.Param("action")
 		message := name + " is " + action
-		c.String(http.StatusOK, message);
+		context.String(http.StatusOK, message);
 	})
+
+	router.GET("/welcome", func(context * gin.Context) {
+		firstName := context.DefaultQuery("firstName", "Guest")
+		queryParam := context.Request.URL.Query()
+		print(queryParam)
+		lastName, pass := context.GetQuery("lastname")
+		if (pass) {
+			context.String(http.StatusOK, "Welcome " + firstName + " " + lastName);
+		} else {
+			context.String(http.StatusOK, "Welcome " + firstName);
+		}
+	})
+
+
 	router.Run(":3000")
 }
